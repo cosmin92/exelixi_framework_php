972da7f5a23cb6e7e89a20c97d6b74998a2b8d58
Cosmin Fane Cozma <cozmacosmin1992@gmail.com>
2019-01-21 00:18:19 +0100

    added Router

---
+++ config/routes.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Author: Cosmin Fane Cozma
+ * Date: 21/01/2019
+ * File: routes.php
+ * Copyright @ 2019 Cosmin Fane Cozma. All rights reserved
+ */
---
+++ core/dispatcher/Router.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cosmin
+ * Date: 21/01/2019
+ * Time: 00:07
+ */
+
+namespace exelixi\dispatcher;
+
+
+class Router
+{
+
+}
